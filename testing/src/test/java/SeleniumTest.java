fd4909d399dadf32693d201ec80ac08a0c57e125
talitha <mlithhs@gmail.com>
2023-11-28 15:47:10 -0300

    refactor: update selenium test to use local driver

--- testing/src/test/java/SeleniumTest.java
+++ testing/src/test/java/SeleniumTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
 
 public class SeleniumTest {
@@ -15,10 +16,17 @@ public class SeleniumTest {
     @Test
     @DisplayName("Should open registration modal.")
     public void shouldOpenRegistrationModal() {
-        WebDriver driver = new FirefoxDriver();
+        String driversDirectory = "src/test/resources/drivers/";
+
+        String geckoDriverPath = new File(driversDirectory, "geckodriver").getAbsolutePath();
+        System.setProperty("webdriver.gecko.driver", geckoDriverPath);
+
         WebDriverManager.firefoxdriver().setup();
 
+        WebDriver driver = new FirefoxDriver();
+
         driver.get("https://fleurspirituelles.github.io/gameteca-quality-assurance/");
+
         WebElement registerButton = driver.findElement(By.id("btnModalCadastro"));
         registerButton.click();
 
@@ -29,5 +37,4 @@ public class SeleniumTest {
 
         driver.quit();
     }
-
 }
